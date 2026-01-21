7ba224f4438f1c9c2e692a80845e58c7f39a141d
Noel Kurian <70000001+nbkurian11@users.noreply.github.com>
2026-01-20 21:17:09 -0500

    Add main.c with OpenGL ES vertex data

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <GLES2/gl2.h>
+
+int main() {
+
+    GLfloat vertices[]= {
+        -0.5f, -0.5f,
+        0.5f, -0.5f, 
+        -0.5f, 0.5f,
+
+        -0.5f, 0.5f,
+        0.5f, -0.5f,
+        0.5f, 0.5f 
+    };
+
+
+    return 0;
+}
